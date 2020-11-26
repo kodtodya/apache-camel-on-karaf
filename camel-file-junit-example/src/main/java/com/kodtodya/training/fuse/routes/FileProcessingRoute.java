c1df92c1c3548b7160390374af9bf8c7d9ac2710
kodtodya <kodtodya@kodtodya-notebook>
2020-11-26 16:38:07 +0530

    junit example added

---
+++ camel-file-junit-example/src/main/java/com/kodtodya/training/fuse/routes/FileProcessingRoute.java
@@ -0,0 +1,22 @@
+package com.kodtodya.training.fuse.routes;
+
+import org.apache.camel.InvalidPayloadException;
+import org.apache.camel.builder.RouteBuilder;
+
+public class FileProcessingRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        onException(InvalidPayloadException.class)
+                .to("log:exceptionLogger");
+
+        // read messages from file & send to file
+        from("file:{{input.file.location}}").routeId("file-reader-route")
+                .to("direct:myDirectComponent").id("direct-producer");
+
+        from("direct:myDirectComponent").routeId("file-writer-route")
+                .to("file:{{output.file.location}}?fileName={{output.file.name}}")
+        .id("file-writer");
+    }
+}
